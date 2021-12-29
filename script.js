3a70d1ce7153187164217c7a13dc4fa3f297c929
Viktoria Niedobova <viktorie.viktorovska@gmail.com>
2021-12-29 18:20:23 +0100

    make upgrade

--- script.js
+++ script.js
@@ -154,3 +154,29 @@ function setReservationSubmit() {
         }
     });
 }
+
+const faders = document.querySelectorAll(".fade-in");
+
+const appearOptions = {
+    threshold: 0.4,
+};
+const appearOnScroll = new IntersectionObserver 
+(function(
+    entries, 
+    appearOnScroll
+) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add("appear");
+            appearOnScroll.unobserve(entry.target);
+        }
+    })
+}, 
+appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+})
+
